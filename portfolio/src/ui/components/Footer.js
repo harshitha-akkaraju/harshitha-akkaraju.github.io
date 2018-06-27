100ab17aef4f27e80f97ad273ea36661da612e62
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-06-26 23:41:09 -0700

    Added footer and icons; yet to implement the redirection

--- portfolio/src/ui/components/Footer.js
+++ portfolio/src/ui/components/Footer.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+// TODO: Delete routes later is a placeholder for now
 import Routes from './../../data/Constants';
-import Intro from '../../data/BriefIntro';
-import './NavBar.css';
+import './Footer.css';
 
+/// Footer component
 class Footer extends Component {
 	render() {
 		return(
-				<div className={"container"}>
-					Fill in the footer
+				<div id={"component"} className={"sticky-bottom"}>
+					<p className={"center"}>© 2018 Harshitha Akkaraju All Rights Reserved</p>
+					<div className={"links"}>
+						<Link to={Routes.home}><img className={"icon"}
+																				src={require('./../../data/footer/github.png')}></img></Link>
+						<Link to={Routes.home}><img className={"icon"}
+																				src={require('./../../data/footer/linkedin.png')}></img></Link>
+						<Link to={Routes.home}><img className={"icon"}
+																				src={require('./../../data/footer/email.png')}></img></Link>
+					</div>
 				</div>
 		);
 	}
--- portfolio/src/ui/containers/HomeScreen.js
+++ portfolio/src/ui/containers/HomeScreen.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import IntroBar from './../components/IntroBar';
 import Section from './Section';
 import Footer from './../components/Footer';
+import './HomeScreen.css'
 
 class HomeScreen extends Component {
 	render() {
 		return(
 				<div>
 					<IntroBar history={this.props.history}/>
-					<Section title={"Organizations"}/>
-					<Section title={"Organizations"}/>
-					<Section title={"Organizations"}/>
+					<div className={"sections"}>
+						<Section title={"Projects"}
+										 history={this.props.history}/>
+						<Section title={"Projects"}
+										 history={this.props.history}/>
+						<Section title={"Projects"}
+										 history={this.props.history}/>
+					</div>
 					<Footer/>
 				</div>
 		)
