cfd00fd49e01f39f0b1bec2aa6ef2e53ebbe5b9f
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-07-12 09:47:11 -0700

    changes to build

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router} from 'react-router';
 import './index.css';
 import App from './ui/containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import createHashHistory from 'history/createHashHistory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
+ReactDOM.render(
+		<Router history={hashHistory}>
+			<App />
+		</Router>,
+		document.getElementById('root')
+);
 registerServiceWorker();
