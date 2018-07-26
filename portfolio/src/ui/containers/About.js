cf2cbcd9e4b609a42ff8a8e32bd94dbd4d8fdfbb
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-07-25 23:55:38 -0700

    changed color palette on the about page

--- portfolio/src/ui/containers/About.js
+++ portfolio/src/ui/containers/About.js
@@ -19,32 +19,35 @@ class About extends Component {
 									 alt={"Me on route to the Grand Coulee Dam in Washington"}/>
 						</div>
 
-						<p className={"about"}>
-							<span className={"quickIntro"}>Hello, there! <br/> I am Harshitha. </span>
+						<div className={"about"}>
+							<p>
+								<span className={"quickIntro"}>Hello, there! <br/> I am Harshitha. </span>
 
-							<span className={"aboutText"}>
+								<span className={"aboutText"}>
 								<br/>
 								I am a senior studying Informatics,
 								combination of systems design and software engineering, at the
 								University of Washington in Seattle. I am driven by the
-								potential technology offers to create new affordances. I am
-								interested in full-stack development and creating solutions using
-								the cloud.
+								potential technology offers to create new affordances. At the moment,
+								I am exploring my interest in cloud computing.
 
 								<br/><br/>
 
-								I am passionate about improving equity in STEM and computing
-								education. As the Outreach Program Director of <Link to={Routes.project}>UW Society of
-								Women Engineers</Link> and the Director of Diversity Efforts of <Link to={Routes.project}>Women in
-								Informatics</Link>, I helped organize several events including
+								I feel strongly about improving equity in STEM and computing
+								education. As a student leader in the UW Society of Women Engineers
+								and Women in Informatics, I helped organize several events including
 								programming workshops, networking events, and a hackathon.
 
 								<br/><br/>
 
-								If you are interested in collaborating with SWE, WINFO or just
-								want to reach out email me at <Link to={Routes.email}>akkarh@uw.edu</Link>.
+								I am always open to getting involved in interesting projects.
+								Email me at <Link to={Routes.email}>akkarh@uw.edu</Link> if you
+								want to collaborate or just want to reach out.
 							</span>
-						</p>
+							</p>
+
+							<button className={'btn btn-secondary'}>Explore work</button>
+						</div>
 					</div>
 					<Footer/>
 				</div>
