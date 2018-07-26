e14247882126448c53f2749fd76f4c48394be409
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-07-25 23:52:07 -0700

    Revamped home page

--- portfolio/src/ui/containers/HomeScreen.js
+++ portfolio/src/ui/containers/HomeScreen.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import IntroBar from './../components/IntroBar';
+import NavBar from './../components/NavBar';
 import Section from './Section';
 import Footer from './../components/Footer';
-import './HomeScreen.css'
+import './HomeScreen.css';
 
 class HomeScreen extends Component {
 	render() {
 		return(
 				<div>
-					<IntroBar history={this.props.history}/>
+					<NavBar history={this.props.history}/>
+					<div className={"text"}>
+						<p className={"pitch"}>
+							I am a design-literate developer studying <span>Software Engineering </span>
+							and <span>Human Computer Interaction</span> at the University of Washington in Seattle.
+						</p>
+						<p className={"currRole"}>
+							I am currently developing research tools at the
+							<a href={'http://faculty.washington.edu/ajko/students'}> Code and Cognition Lab</a>.
+							In the past, I worked with the
+							<a href={'https://tcat.cs.washington.edu/'}> Taskar Center for Accessible Technology </a>
+							in the Paul G. Allen School of Computer Science.
+						</p>
+					</div>
 					<div className={"sections"}>
 						<Section title={"Selected Projects"}
 										 history={this.props.history}/>
