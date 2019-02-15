1e553a820dad8e6b2d4f1e09d9defc515ec27b83
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-02-14 22:02:13 -0800

    Edited homescreen appearence

--- portfolio/src/ui/containers/HomeScreen.js
+++ portfolio/src/ui/containers/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './../components/NavBar';
+import Headshot from './../components/Headshot';
 import Section from './Section';
 import Footer from './../components/Footer';
 import './HomeScreen.css';
@@ -11,28 +12,20 @@ class HomeScreen extends Component {
 		return(
 				<div>
 					<NavBar history={this.props.history}/>
-					<div className={"text"}>
-						<p className={"pitch"}>Hello, there!</p>
-						<p className={"pitch"}>
-							I am a <span>design-literate developer</span> studying <span className={"standout"}>Software Engineering </span>
-							and <span className={"standout"}>Human Computer Interaction</span> at the University of Washington in Seattle.
-						</p>
-						<p className={"currRole"}>
-							I am currently engineering research tools at the
-							<a href={'http://faculty.washington.edu/ajko/students'}> Code and Cognition Lab</a> directed by Dr. Andrew J. Ko.
+					<div className="home-container">
+						<div className="home-body">
+							<div className="home-headshot"><Headshot /></div>
+							<p className={"pitch"}>
+								I am a senior at the University of Washington Seattle studying Informatics - a combination of systems design and software engineering.
+								<br /><br />
 
-							<br/>
+								I am developing research tools at the
+								<a href={'http://faculty.washington.edu/ajko/students'}> Code and Cognition Lab</a> directed by Dr. Andrew J. Ko. During Summer 2019, I will be working with PayPal as a Software Engineering Intern in the Bay Area.
 
-							I am also developing software for <a href={'http://depts.washington.edu/melab/projects/project-emar/'}>Project EMAR</a>.
-						</p>
+								<br /><br />
 
-					</div>
-					<div className={"sections"} id={"selectedProjects"}>
-						<Section title={"Selected Projects"}
-										 history={this.props.history}/>
-						<br/>
-						<div className={'sandbox'}>
-							<Link to={Routes.sandbox}><button className={'btn btn-secondary'}>See Sandbox</button></Link>
+								I feel strongly about improving equity in STEM and computing education. I take part in outreach work through Society of Women Engineers (SWE) and Women in Informatics.
+							</p>
 						</div>
 					</div>
 					<Footer/>
