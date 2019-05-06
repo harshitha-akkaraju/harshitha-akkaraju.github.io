4e4708677e34071e51044596b44ca7eb97ba75d7
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-05-06 15:36:29 -0700

    removed dedicated pages/views for projects

--- portfolio/src/ui/containers/App.js
+++ portfolio/src/ui/containers/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import {HashRouter as Router, Switch, Redirect, Route} from 'react-router-dom';
 import Routes from './../../data/Constants';
 import HomeScreen from './HomeScreen';
-import About from './About';
-import ProjectView from './ProjectView';
 import SandboxView from './SandboxView';
 import './App.css';
 
@@ -15,8 +13,6 @@ class App extends Component {
 				<Router>
 					<Switch>
 						<Route exact path={Routes.home} component={HomeScreen} />
-						<Route exact path={Routes.about} component={About} />
-						<Route exact path={Routes.project} component={ProjectView} />
 						<Route exact path={Routes.sandbox} component={SandboxView} />
 						<Redirect to="/" />
 					</Switch>
