78e76c54e203cf9bc7c8905a3a9fc4ed2442fcc0
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-06-15 15:56:43 -0700

    Updated link experience tab in App.js

--- portfolio/src/ui/containers/App.js
+++ portfolio/src/ui/containers/App.js
@@ -3,6 +3,7 @@ import {HashRouter as Router, Switch, Redirect, Route} from 'react-router-dom';
 import Routes from './../../data/Constants';
 import HomeScreen from './HomeScreen';
 import SandboxView from './SandboxView';
+import Experience from './Experience';
 import './App.css';
 
 /// All the other components are wrapped in this class
@@ -14,6 +15,7 @@ class App extends Component {
 					<Switch>
 						<Route exact path={Routes.home} component={HomeScreen} />
 						<Route exact path={Routes.sandbox} component={SandboxView} />
+						<Route exact path={Routes.about} component={Experience} />	
 						<Redirect to="/" />
 					</Switch>
 				</Router>
